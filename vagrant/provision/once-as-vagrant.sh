e29a0fad0d318f9f1e1f207e309bf6dd8ff05ba3
Ghazi Triki <ghazi.triki@riadvice.tn>
2021-09-16 15:57:16 +0100

    Fix chamilo download and php-fpm service restart.

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -8,10 +8,9 @@ info "Provision-script user: `whoami`"
 
 info "Install project dependencies"
 cd /app
-wget https://github.com/chamilo/chamilo-lms/releases/download/v1.11.16/chamilo-1.11.16.zip
+wget -c https://github.com/chamilo/chamilo-lms/releases/download/v1.11.16/chamilo-1.11.16.zip
 unzip chamilo-1.11.16.zip
 mv chamilo-1.11.16 chamilo
-# composer --no-progress --prefer-dist install
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
