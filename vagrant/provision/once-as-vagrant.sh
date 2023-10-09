fb022711cf29e55cc039f81bd951c4aa4c3bf4df
Yannick Warnier <ywarnier@users.noreply.github.com>
2023-10-09 11:44:32 +0200

    Update to latest stable v1.11.26 (security patches included)

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -8,9 +8,9 @@ info "Provision-script user: `whoami`"
 
 info "Install project dependencies"
 cd /app
-wget -c https://github.com/chamilo/chamilo-lms/releases/download/v1.11.16/chamilo-1.11.16.zip
-unzip chamilo-1.11.16.zip
-mv chamilo-1.11.16 chamilo
+wget -c https://github.com/chamilo/chamilo-lms/releases/download/v1.11.26/chamilo-1.11.26.zip
+unzip chamilo-1.11.26.zip
+mv chamilo-1.11.26 chamilo
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
